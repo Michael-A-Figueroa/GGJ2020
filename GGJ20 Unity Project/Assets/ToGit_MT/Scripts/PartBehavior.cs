5609bbad81876232e790afddd7a85aa9779a6ba0
Caleb B <cmbraggs@gmail.com>
2020-02-01 17:25:12 -0600

    Bringing your stuff back

---
+++ GGJ20 Unity Project/Assets/ToGit_MT/Scripts/PartBehavior.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PartBehavior : MonoBehaviour
+{
+    public int partType;
+
+    public float partHealth = 3;
+
+    private void Update()
+    {
+        
+    }
+
+    //public void Highlight()
+    //{
+    //    startColor = GetComponent<Renderer>().material.color;
+    //    GetComponent<Renderer>().material.color = Color.yellow;
+    //}
+}

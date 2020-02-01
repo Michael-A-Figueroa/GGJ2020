7e7c584fd9983d2eab4f95276ff67a77bf127b8a
Matthew Moseby <matthew.moseby@gmail.com>
2020-02-01 17:59:34 -0600

    Added Audio Ranomizer
    Added audio randomizer that should be ready for player character audio clips.

---
+++ GGJ20 Unity Project/Assets/ToGit_MT/Scripts/PlayerAudio.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAudio : MonoBehaviour
+{
+    public AudioSource audioSource;
+    public AudioClip[] audioClips;
+
+    public float clipWaitTime = 5;
+
+    public bool playClips;
+
+    public void PlayClipLoop()
+    {
+        playClips = true;
+        Invoke("RandomAudioClip", clipWaitTime);
+    }
+
+    public void StopClipLoop()
+    {
+        playClips = false;
+    }
+
+    void RandomAudioClip()
+    {
+        if (playClips)
+        {
+            audioSource.clip = audioClips[Random.Range(0, audioClips.Length)];
+            audioSource.Play();
+            PlayClipLoop();
+        }
+    }
+}

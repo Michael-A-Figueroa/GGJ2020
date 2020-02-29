cef63ed3f07d30db4443ec76c1c6fc0e607fcc12
Caleb B <cmbraggs@gmail.com>
2020-02-28 20:26:13 -0600

    Updated Files
    This has the Updated Files located in GIT_ST_FX_VS_001. Standalone File import onto Desktop and Open using Unity HUB

---
+++ GIT_ST_FX_VS_001/Root Folder/Street Fixer 2/Assets/Scripts/ChangeSceneAuto.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeSceneAuto : MonoBehaviour
+{
+    public AudioClip newAudio;
+
+   public void ChangeScene(string newScene)
+    {
+        FindObjectOfType<AudioManager>().OnSceneChange(newAudio);
+        SceneManager.LoadScene(newScene);
+    }
+}
